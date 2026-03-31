9e5caac43fa870c27b316eaa4c24aeebbcf5b0e2
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-03-31 11:05:45 +0900

    トップページ表示

---
+++ test/controllers/groups_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class GroupsControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get groups_create_url
+    assert_response :success
+  end
+end
