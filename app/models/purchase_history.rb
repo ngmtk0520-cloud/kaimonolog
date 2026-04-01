082cd8c9babdc2a915a2efe037730069e93281c8
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-04-01 15:42:09 +0900

    購入履歴　単体テストOK

--- app/models/purchase_history.rb
+++ app/models/purchase_history.rb
@@ -1,4 +1,5 @@
 class PurchaseHistory < ApplicationRecord
   belongs_to :item
   belongs_to :group
+  validates :bought_at, presence: true
 end
--- spec/models/purchase_history_spec.rb
+++ spec/models/purchase_history_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseHistory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:group) { Group.create!(name: "テスト家族", invite_token: "abcdef123456") }
+  let(:item) { Item.create!(name: "牛乳", group: group, kind: :regular) }
+
+  it "商品、グループ、購入日があれば有効であること" do
+    history = PurchaseHistory.new(item: item, group: group, bought_at: Time.current)
+    expect(history).to be_valid
+  end
+
+  it "購入日（bought_at）がなければ無効であること" do
+    history = PurchaseHistory.new(item: item, group: group, bought_at: nil)
+    expect(history).to_not be_valid
+  end
+
+  it "商品（item）がなければ無効であること" do
+    history = PurchaseHistory.new(item: nil, group: group, bought_at: Time.current)
+    expect(history).to_not be_valid
+  end
 end
