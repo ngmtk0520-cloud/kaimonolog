fb6888e638be691bc33db80f6b2ee2915a0da30c
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-04-01 12:12:48 +0900

    カテゴリーモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_31_063108) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_01_031115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_categories_on_group_id"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name", null: false
@@ -49,5 +57,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_31_063108) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "groups"
   add_foreign_key "items", "groups"
 end
