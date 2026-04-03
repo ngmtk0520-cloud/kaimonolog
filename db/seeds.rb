a88049b59b57e25ee0b0e8af5c5af9ced9a117a1
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-04-04 00:01:20 +0900

    seed編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ group = Group.find_or_create_by!(name: "長松家")
 
 group.categories.find_or_create_by!(name: "通常購入")
 group.categories.find_or_create_by!(name: "定期購入")
-group.categories.find_or_create_by!(name: "スポット")
+group.categories.find_or_create_by!(name: "スポット購入")
