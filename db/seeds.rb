12272c3775674c84338c1cc3b685cdcb1f18ca7c
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-04-03 23:10:54 +0900

    本番環境にデータベース追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+group = Group.find_or_create_by!(name: "長松家")
+
+group.categories.find_or_create_by!(name: "通常購入")
+group.categories.find_or_create_by!(name: "定期購入")
+group.categories.find_or_create_by!(name: "スポット")
