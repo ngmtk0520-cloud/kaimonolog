8daa75487fef5c96e0d6127da69876d6f40317ca
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-04-09 16:39:34 +0900

    グループに誰もいなくなったら全て消える

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :group, optional: true
+  after_destroy :destroy_empty_group
+
   has_many :purchase_histories
 
   validates :nickname, presence: true
+
+  private
+
+  def destroy_empty_group
+    # ユーザーが所属していたグループを取得
+    return if group.nil?
+    
+    # そのグループに所属しているユーザーが0人になったら、グループを削除
+    if group.users.count == 0
+      group.destroy
+    end
+  end
 end
